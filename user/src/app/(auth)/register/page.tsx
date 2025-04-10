27910284eb8ec3702940b5d51c4b81a3ef7afacf
nguyen <125894255+glexiv808@users.noreply.github.com>
2025-04-10 22:03:29 +0700

    refactor: UI login/register user/admin/owner
    * Fe login/register user
    * FE login/register admin/owner
    * Fix FE
    * docs: file .env
    
    Co-authored-by: Tuấn Anh <138606608+HoangTuanAnh03@users.noreply.github.com>

--- user/src/app/(auth)/register/page.tsx
+++ user/src/app/(auth)/register/page.tsx
@@ -11,27 +11,24 @@ import RegisterForm from "@/app/(auth)/register/register-form";
 
 export default function RegisterPage() {
   return (
-    <div className="flex mt-2 w-full justify-center text-[#121212]">
-      <div className=" flex flex-col max-w-[1340px] w-full">
+    <div className="flex w-full justify-center text-[#121212] bg-cover"
+        style={{
+        backgroundImage: "url('/image.png?height=1080&width=1920')",
+      }}
+    >
+      <div className="absolute inset-0 bg-teal-500/30 backdrop-blur-sm h-full"></div>
+      
+      <div className="max-w-[600px] w-full h-screen flex justify-center items-center">
+      <div className="w-full max-w-md p-8 rounded-lg bg-sky-50/90 shadow-lg relative z-10">
         <div className="flex items-center my-6">
-          <h3 className="text-xl font-bold">Chào mừng bạn đến với</h3>
-          <Image
-            className="pl-2"
-            src={"/logo_black.png"}
-            width={80}
-            height={30}
-            alt="Logo"
-          />
+          <h3 className="text-xl font-bold">Chào mừng bạn đến với AE booking</h3>
         </div>
         <div className="flex justify-between w-full gap-[10%] ">
           <div className="flex-[4_4_0%]">
-            <div className="text-3xl font-bold">Đăng ký tài khoản</div>
-            <TermsGoogle/>
-            {/* <Google title={"Đăng ký bằng Google"} /> */}
+            <div className="text-2xl font-bold">Đăng ký tài khoản - Khách </div>
             <div className="relative flex items-center py-4">
               <Separator className="absolute" />
               <div className="relative flex w-full  justify-center  ">
-                <span className="bg-background px-3 text-sm">hoặc</span>
               </div>
             </div>
             <RegisterForm />
@@ -45,18 +42,10 @@ export default function RegisterPage() {
               </Link>
             </div>
           </div>
-          <div className="flex-[5_5_0%] ">
-            <div className="flex w-full justify-center h-full items-center">
-              <Image
-                src={"/robby-login.png"}
-                width={320}
-                height={310}
-                alt="Robby Logo"
-              />
-            </div>
-          </div>
+          
         </div>
       </div>
+    </div>
     </div>
   );
 }
